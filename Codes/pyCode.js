96683110ad86ba1f046b0284ea095b79917d6c0a
Rudra <prajapatirudra127@gmail.com>
2023-12-19 02:47:56 +0530

    python done

---
+++ Codes/pyCode.js
@@ -0,0 +1,39 @@
+    import { fileURLToPath } from 'url';
+    import { exec } from 'child_process';
+    import path from 'path';
+    import compiler from 'compilex';
+    import fs from 'fs/promises';
+
+    const currentModulePath = fileURLToPath(import.meta.url);
+    const currentModuleDir = path.dirname(currentModulePath);
+
+    const runPythonCode = async (code) => {
+        try {
+          const fullPath = path.resolve(currentModuleDir, 'python_script.py');
+          await fs.writeFile(fullPath, code);
+      
+          return new Promise((resolve, reject) => {
+      
+                const pythonCommand = `python "${fullPath}"`;
+                console.log('Executing Python Command:', pythonCommand);
+      
+                const child = exec(pythonCommand, (error, stdout, stderr) => {
+                  console.log('Python Execution Output:', { stdout, stderr });  
+      
+                  if (error) {
+                    console.error('Python Execution Error:', error);
+                    resolve({ output: error.message ? error.message.trim() : '' });
+                  } else {
+                    resolve({ output: stdout.trim() });
+                  }
+                });
+             
+
+          });
+        } catch (error) {
+          console.error(`Error writing Python code to file: ${error}`);
+          throw new Error(`Error writing Python code to file: ${error}`);
+        }
+      };
+      
+    export default runPythonCode;
--- server.js
+++ server.js
@@ -1,50 +1,88 @@
-import express from "express";
-import { config as dotenvConfig } from "dotenv";
-import { connectToMongo } from "./db.js";
-import cors from "cors";
-import runCode from "./Code/code.js";
+  import express from "express";
+  import { config as dotenvConfig } from "dotenv";
+  import { connectToMongo } from "./db.js";
+  import cors from "cors";
+  import runCode from "./Codes/jsCode.js";
+  import compileAndRunCCode from "./Codes/cCode.js";
+  import runPythonCode from "./Codes/pyCode.js";
 
-dotenvConfig();
+  dotenvConfig();
 
-// Connected to the database
-const db = connectToMongo();
-const app = express();
-const port = 6969;
+  // Connected to the database
+  const db = connectToMongo();
+  const app = express();
+  const port = 6969;
 
-app.use(express.json());
+  app.use(express.json());
 
-app.use(
-  cors({
-    origin: "*", // Allow all origins during development
-    methods: ["GET", "POST", "UPDATE", "DELETE", "PUT", "PATCH", "OPTIONS"],
-    credentials: true,
-  })
-);
+  app.use(
+    cors({
+      origin: "*", // Allow all origins during development
+      methods: ["GET", "POST", "UPDATE", "DELETE", "PUT", "PATCH", "OPTIONS"],
+      credentials: true,
+    })
+  );
 
-// Testing endpoint
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the IDE Project API Server!" });
-});
+  // Testing endpoint
+  app.get("/", (req, res) => {
+    res.json({ message: "Welcome to the IDE Project API Server!" });
+  });
 
-// Endpoint for running code
-app.post("/run", async (req, res) => {
-  const { code } = req.body;
+  // Endpoint for running code
+  app.post("/run", async (req, res) => {
+    const { code } = req.body;
 
-  console.log("Received code:", code); // Log the received code
+    console.log("Received code:", code); // Log the received code
 
-  try {
-    const { result, consoleOutput } = await runCode(code);
+    try {
+      const { result, consoleOutput } = await runCode(code);
 
-    console.log("Code result:", result);
-    console.log("Console Output:", consoleOutput);
+      console.log("Code result:", result);
+      console.log("Console Output:", consoleOutput);
 
-    res.json({ success: true, result, consoleOutput });
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ success: false, error: error.toString() });
-  } 
-});
+      res.json({ success: true, result, consoleOutput });
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ success: false, error: error.toString() });
+    } 
+  });
+  app.post("/run-c", async (req, res) => {
+    const { code } = req.body;
 
-app.listen(port, () => {
-  console.log(`IDE app listening on port http://localhost:${port}`);
-});
+    console.log("Received C code:", code); 
+
+    try {
+      const cResult = await compileAndRunCCode(code);
+
+      console.log("C Code Result:", cResult);
+
+      res.json({ success: true, cResult });
+    } catch (error) {
+      console.error("Error compiling and running C code:", error);
+      res.status(500).json({ success: false, error: error.toString() });
+    }
+  });
+
+  app.post("/run-python", async (req, res) => {
+    const { code } = req.body;
+  
+    console.log("Received Python code:", code);
+  
+    try {
+      const pythonResult = await runPythonCode(code);
+  
+      console.log("Python Code Result:", pythonResult);
+  
+      res.json({ success: true, pythonResult });
+    } catch (error) {
+      console.error("Error running Python code:", error);
+  
+      // Use error.message or JSON.stringify(error) for a more informative error response
+      res.status(500).json({ success: false, error: error.message || JSON.stringify(error) });
+    }
+  });
+  
+
+  app.listen(port, () => {
+    console.log(`IDE app listening on port http://localhost:${port}`);
+  });
