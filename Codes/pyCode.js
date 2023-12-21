63a06e67f24ea33fad0d30c4dca7585d7ec56f34
Rudra <prajapatirudra127@gmail.com>
2023-12-21 20:43:43 +0530

    .

--- Codes/pyCode.js
+++ Codes/pyCode.js
@@ -1,39 +1,39 @@
-    import { fileURLToPath } from 'url';
-    import { exec } from 'child_process';
-    import path from 'path';
-    import compiler from 'compilex';
-    import fs from 'fs/promises';
+import { exec } from 'child_process';
+import path from 'path';
+import compiler from 'compilex';
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
 
-    const currentModulePath = fileURLToPath(import.meta.url);
-    const currentModuleDir = path.dirname(currentModulePath);
+const currentModulePath = fileURLToPath(import.meta.url);
+const currentModuleDir = path.dirname(currentModulePath);
 
-    const runPythonCode = async (code) => {
-        try {
-          const fullPath = path.resolve(currentModuleDir, 'python_script.py');
-          await fs.writeFile(fullPath, code);
+const runPythonCode = async (code) => {
+  try {
+    const fullPath = path.resolve(currentModuleDir, 'python_script.py');
+    await fs.writeFile(fullPath, code);
+    
+    return new Promise((resolve, reject) => {
       
-          return new Promise((resolve, reject) => {
+      const pythonCommand = `python "${fullPath}"`;
+      console.log('Executing Python Command:', pythonCommand);
       
-                const pythonCommand = `python "${fullPath}"`;
-                console.log('Executing Python Command:', pythonCommand);
-      
-                const child = exec(pythonCommand, (error, stdout, stderr) => {
-                  console.log('Python Execution Output:', { stdout, stderr });  
-      
-                  if (error) {
-                    console.error('Python Execution Error:', error);
-                    resolve({ consoleOutput: error.message ? error.message.trim() : '' });
-                  } else {
-                    resolve({ consoleOutput: stdout.trim() });
-                  }
-                });
-             
-
-          });
-        } catch (error) {
-          console.error(`Error writing Python code to file: ${error}`);
-          throw new Error(`Error writing Python code to file: ${error}`);
+      const child = exec(pythonCommand, (error, stdout, stderr) => {
+        console.log('Python Execution Output:', { stdout, stderr });  
+        
+        if (error) {
+          console.error('Python Execution Error:', error);
+          resolve({ consoleOutput: error.message ? error.message.trim() : '' });
+        } else {
+          resolve({ consoleOutput: stdout.trim() });
         }
-      };
-      
-    export default runPythonCode;
+      });
+    });
+} 
+  catch (error) {
+      console.error(`Error writing Python code to file: ${error}`);
+      throw new Error(`Error writing Python code to file: ${error}`);
+    }
+};
+
+export default runPythonCode;
+
